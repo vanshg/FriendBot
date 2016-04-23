6f41c158d593e070a5e5a99782d4402603674a61
Vansh Gandhi <vansh.gandhi@gmail.com>
2016-04-23 11:51:08 -0700

    Mongo

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var request = require('request')
-var storage = require('mongodb').MongoClient
+var MongoClient = require('mongodb').MongoClient
     , assert = require('assert');
 var app = express()
 
 var url = "mongodb://root:Nishank@ds019101.mlab.com:19101/friendbotdb"
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+
+  db.close();
+});
 
 app.set('port', (process.env.PORT || 5000))
-storage.initSync();
 
 // Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
