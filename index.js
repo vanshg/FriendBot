2a897d2c6d26a677b6d6884ea7f69aa731ac0c64
Aneel Yelamanchili <aneel@yelamanchili.io>
2016-04-23 23:06:57 -0700

    small change

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ app.post('/webhook/', function (req, res) {
                 pairUser(sender)
             }
             else if(text.startsWith("@quit ")) {
+                console.log("Got here")
                 unpairUser(sender)
             }
             sendFriendMessage(sender, text)
